a67daefb258f309795cd8e16dc81d0224c5300cb
suzukikeita <keita.suzukip3@gmail.com>
2023-10-30 20:59:27 +0900

    New: create invoice api:tada:

--- src/app.js
+++ src/app.js
@@ -2,16 +2,31 @@ const express = require('express')
 const createError = require('http-errors')
 const app = express()
 const port = 8080;
+const clientRepository = require('./shared/repositories/clients');
 const invoiceRepository = require('./shared/repositories/invoices');
-const { DEFAULT_LIMIT, DEFAULT_OFFSET } = require('./shared/constant');
-const { passwordVerify } = require('./shared/middlewares');
+const dayjs = require('dayjs');
+const timezone = require('dayjs/plugin/timezone');
+const utc = require('dayjs/plugin/utc');
+const { DEFAULT_LIMIT, DEFAULT_OFFSET, INVOICE_STATUS, DEFAULT_TIME_ZONE, TAX_RATE, COMISSION_RATE } = require('./shared/constant');
+const { passwordVerify, addNow } = require('./shared/middlewares');
+const uuid = require('uuid');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault(DEFAULT_TIME_ZONE);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
 app.use(passwordVerify)
+app.use(addNow)
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// 請求書一覧取得
 app.get('/api/invoices', async (req, res) => {
   const { limit = DEFAULT_LIMIT, offset = DEFAULT_OFFSET } = req.query;
   const { companyId } = req._context.user
@@ -19,6 +34,45 @@ app.get('/api/invoices', async (req, res) => {
   // ユーザーが所属している企業の請求書で絞る
   const invoices = await invoiceRepository.findAll({ where: { companyId }, limit, offset });
   res.json(invoices);
+})
+
+// 請求書作成
+app.post('/api/invoices', async (req, res) => {
+  const { user, now } = req._context;
+  const { clientId, paymentAmount } = req.body;
+  // clientIdがstring, paymentAmountがnumberでなければエラー
+  if (typeof clientId !== 'string' || typeof paymentAmount !== 'number') {
+    res.status(400).json({ message: 'invalid request.' });
+    return;
+  }
+  // ユーザーが所属している企業のクライアントであるかをチェックする
+  const conditions = { where: { clientId, companyId: user.companyId }, raw: true }
+  const client = await clientRepository.findOne(conditions);
+  if (!client) {
+    res.status(400).json({ message: 'client is not found.' });
+    return;
+  }
+  // 支払金額 に手数料4%を加えたものに更に手数料の消費税を加えたものを請求金額とする
+  const invoiceAmount = paymentAmount + (paymentAmount * COMISSION_RATE * (1 + TAX_RATE));
+  const tax = invoiceAmount * TAX_RATE;
+  const commission = invoiceAmount * COMISSION_RATE;
+  const params = {
+    invoiceId: uuid.v4(),
+    companyId: user.companyId,
+    clientId,
+    issuedAt: dayjs(now).toDate(),
+    commission,
+    commissionRate: COMISSION_RATE,
+    tax,
+    taxRate: TAX_RATE,
+    invoiceAmount,
+    paymentAmount,
+    status: INVOICE_STATUS.pending,
+    // 支払い期限は発行日から1ヶ月後とする
+    paymentDeadline: dayjs(now).add(1, 'month').endOf('day').toDate(),
+  };
+  await invoiceRepository.create(params);
+  res.status(201).end();
 })
 
 // view engine setup
