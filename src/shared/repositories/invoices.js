10d8f6aa8915629e1edfcefd2837acce33aa9695
suzukikeita <keita.suzukip3@gmail.com>
2023-10-30 20:58:08 +0900

    New: create invoice repository:tada:

--- src/shared/repositories/invoices.js
+++ src/shared/repositories/invoices.js
@@ -8,6 +8,27 @@ const findAll = async (conditions = {}) => {
   return await Invoice.findAndCountAll(conditions);
 };
 
+/**
+ * 請求書情報を作成する
+ * @param {string} params.invoiceId
+ * @param {string} params.clientId
+ * @param {string} params.companyId
+ * @param {number} params.issuedAt
+ * @param {number} params.commission
+ * @param {number} params.commissionRate
+ * @param {number} params.tax
+ * @param {number} params.taxRate
+ * @param {number} params.invoiceAmount
+ * @param {number} params.paymentAmount
+ * @param {status} params.status
+ * @param {number} params.paymentDeadline
+ * @return {Promise<{ rows: Invoice[]; count: number }>}
+ */
+const create = async (params = {}) => {
+  return await Invoice.create(params);
+};
+
 module.exports = {
   findAll,
+  create,
 };
